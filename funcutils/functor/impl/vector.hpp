f97e26861f6eb25ef86d8385b99edf814959e249
dgehringer <{ID}+{username}@users.noreply.github.com>
2024-08-18 11:53:41 +0200

    :tada: initial commit

--- funcutils/functor/impl/vector.hpp
+++ funcutils/functor/impl/vector.hpp
@@ -6,29 +6,29 @@
 #define FUNCUTILS_FUNCTOR_VECTOR_IMPL_HPP
 
 #include "funcutils/functor/functor.hpp"
+#include "funcutils/utils.hpp"
+#include <vector>
+#include <utility>
 
 namespace funcutils::core {
 
+template <template <class...> class F> struct _functor_impl;
+
 template <> struct _functor_impl<std::vector> {
-  
-  template <class AA, class B, class Function, class A = remove_reference_t<AA>>
+
+  template <class AA, class B, class Function,
+            class A = std::remove_reference_t<AA>>
   static constexpr decltype(auto) fmap(Function &&f) {
     return [&f](std::vector<A> &&arg) -> std::vector<B> {
       std::vector<B> result(arg.size());
       for (size_t i = 0; i < arg.size(); ++i) {
-        if constexpr (std::is_move_assignable_v<B> ||
-                      std::is_trivially_move_assignable_v<B>) {
-          result[i] = std::move(f(std::forward<A>(arg[i])));
-        } else if constexpr (std::is_copy_assignable_v<B> ||
-                             std::is_trivially_copy_assignable_v<B>) {
-          result[i] = f(arg[i]);
-        }
+          result[i] = call<AA, B, Function>(std::forward<Function>(f), std::forward<A>(arg[i]));
       }
       return result;
     };
   }
 };
-  
-}
+
+} // namespace funcutils::core
 
 #endif // FUNCUTILS_FUNCTOR_VECTOR_IMPL_HPP
