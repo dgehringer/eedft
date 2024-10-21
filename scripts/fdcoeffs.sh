cbdab6083943224f3a06818a9d9d4645a7f2b407
Dominik Gehringer <35268573+dgehringer@users.noreply.github.com>
2024-10-21 21:29:28 +0200

    🧑‍💻 update fd stencil generator args to fit new structure

--- scripts/fdcoeffs.sh
+++ scripts/fdcoeffs.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-python3 codegen/fdcoeffs.py -o include/eedft/core/stencil/data.hpp --min-order=2 --max-order=14 --namespace "eedft::core::stencil"
+python3 codegen/fdcoeffs.py -o include/eedft/core/stencil/data.h --min-order=2 --max-order=14 --namespace "eedft::core::fd"
